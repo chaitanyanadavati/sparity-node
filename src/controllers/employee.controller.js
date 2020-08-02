6a8d98bc72c6c0ff90324d35553f94944d791c57
chaitanya nadavati <nadavatichaitanya@gmail.com>
2020-08-02 17:38:18 +0530

    serarch added

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -6,10 +6,20 @@ module.exports = {
     getEmployees: async (req, res, next) => {
         const skip = +req.query.skip || 0,
             limit = +req.query.limit || 10;
+        const search_string = req.query.search_string || undefined;
+        let filter = {};
+        if (search_string) {
+            const search_query = {
+                $or: [
+                    { firstName: { $regex: search_string, $options: 'i' } },
+                ]
+            };
+            filter = { ...filter, ...search_query };
+        }
         try {
             const options = { skip, limit };
             const total = await UsersModel.countDocuments({});
-            const employees = await UsersModel.find({}, {}, options);
+            const employees = await UsersModel.find(filter, {}, options);
             const respObj = { skip, limit, total, employees };
             return res.status(http_status.OK).send(respObj);
         } catch (error) {
